5f62aa8e395f20e5e3eec91e402527f078379598
aleksandr.morzeev <aleksandr.morzeev@t-systems.com>
2022-08-22 12:31:04 +0300

    Refactor login testcase

--- src/test/java/org/example/LoginTest.java
+++ src/test/java/org/example/LoginTest.java
@@ -7,15 +7,22 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.By;
+import org.slf4j.Logger;
 
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.sleep;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.sleep;
+import static com.codeborne.selenide.Selenide.open;
 
+//@Slf4j
 @ExtendWith({MyAllureSetup.class, /*MySelenoidSetup.class*/})
 public class LoginTest {
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(LoginTest.class);
+    private final String USERNAME = "aleksandr.morzeev@t-systems.com";
+
     @BeforeEach
     @Step("Setup Webdriver")
     public void init() {
@@ -28,10 +35,8 @@ public class LoginTest {
     @Test
     @Description("Login positive test")
     public void userCanLogin() {
-        loginAs("aleksandr.morzeev@t-systems.com", "DigitalTwin731!)");
-        sleep(5000);
-
-
+        loginAs(USERNAME, "DigitalTwin731!)");
+        sleep(3000);
     }
 
     @Step("Login as {username}")
@@ -39,6 +44,8 @@ public class LoginTest {
         $(By.id("username")).shouldBe(visible).setValue(username);
         $(By.id("password")).shouldBe(visible).setValue(password);
         $(By.id("kc-login")).shouldBe(visible).click();
-        System.out.println("Login as " + username);
+        $(".icon-user").shouldBe(visible);
+        $(".navigation-bar-item--right").shouldHave(text(USERNAME));
+        log.info("Login as {}", username);
     }
 }
